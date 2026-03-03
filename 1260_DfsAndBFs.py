ca662ed539f7f5235ea521c7f7ea373c100cde86
Siyeong Cheon <108331642+1000c0@users.noreply.github.com>
2026-03-03 15:10:28 +0900

    1260 solve

--- 1260_DfsAndBFs.py
+++ 1260_DfsAndBFs.py
@@ -1,5 +1,37 @@
 from collections import deque
 
 n, m, v = map(int, input().split())
+dfs_visited = [False] * (n+1)
+bfs_visited = [False] * (n+1)
+graph = [[] for _ in range(n+1)]
 dfs_ans = []
 bfs_ans = []
+
+for _ in range(m):
+  a, b = map(int, input().split())
+  graph[a].append(b)
+  graph[b].append(a)
+
+for i in range(n+1):
+  graph[i].sort()
+
+def dfs(x):
+  dfs_visited[x] = True
+  dfs_ans.append(x)
+  for nx in graph[x]:
+    if not dfs_visited[nx]:
+      dfs(nx)
+
+que = deque([v])
+bfs_visited[v] = True
+while que:
+  x = que.popleft()
+  bfs_ans.append(x)
+  for nx in graph[x]:
+    if not bfs_visited[nx]:
+      bfs_visited[nx] = True
+      que.append(nx)
+dfs(v)
+
+print(' '.join(map(str, dfs_ans)))
+print(' '.join(map(str, bfs_ans)))
